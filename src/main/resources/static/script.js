d6025c8a4c06700edc5bdd815055a33bd647d337
NikoNovsky <nikonovsky@gmail.com>
2026-02-09 23:32:51 +0100

    added script.js, style.css, index.html

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,14 @@
+const bootstrapScript = document.createElement("script");
+bootstrapScript.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js";
+bootstrapScript.integrity = "sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm";
+bootstrapScript.crossOrigin = "anonymous";
+
+let tableAndForms = document.getElementById("tableAndForms");
+
+document.getElementById("loadAllMovies").addEventListener("click", () => {
+    if (tableAndForms.style.display === "none") {
+        tableAndForms.style.display = "block";
+    }
+});
+
+document.body.appendChild(bootstrapScript);
