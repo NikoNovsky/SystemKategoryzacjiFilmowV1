0bfe897de6e0c06ca89f8eedb8285fac9c682aeb
NikoNovsky <nikonovsky@gmail.com>
2026-02-10 15:05:07 +0100

    login fix for all users

--- src/main/java/com/example/currenda/config/WebSecurityConfig.java
+++ src/main/java/com/example/currenda/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/index", "/css/**", "/script/**", "/login", "/error").permitAll()
+                        .requestMatchers("/", "/index", "/css/**", "/script/**", "/login", "/error", "/.well-known/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
